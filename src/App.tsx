2f1594ef59855afae3bca0425d2c4411c21882e3
Aoi <ht.so.ik0102@gmail.com>
2021-07-05 00:14:42 +0700

    create Home.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Switch, Route, useLocation } from 'react-router-dom';
 
 import Nav from "./components/Nav";
+import Home from './pages/Home';
 
 
 function App() {
+  const location = useLocation();
+
   return (
     <div>
       <Nav />
+      <Switch location={location} key={location.pathname}>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+      </Switch>
     </div>
   );
 }
