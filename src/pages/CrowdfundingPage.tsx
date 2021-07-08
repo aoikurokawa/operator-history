00599f3e34a1225f5858b178e6bd93b3aae80b31
Aoi <ht.so.ik0102@gmail.com>
2021-07-08 20:55:07 +0700

    create carousel

--- src/pages/CrowdfundingPage.tsx
+++ src/pages/CrowdfundingPage.tsx
@@ -1,51 +1,42 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
+
+import Carousels from "../components/Carousel";
+import { Container } from "@material-ui/core";
 
 const useStyles = makeStyles({
   root: {
     maxWidth: 345,
   },
+  carouselContainer: {
+    // width: "10rem",
+    padding: "2rem",
+    height: "20rem",
+  },
+  buttonContainer: {
+    display: "flex", 
+    justifyContent: "space-evenly",
+  }
 });
 
 const CrowdfundingPage = () => {
   const classes = useStyles();
 
   return (
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          alt="Contemplative Reptile"
-          height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-      </CardActions>
-    </Card>
+    <>
+      <Container className={classes.carouselContainer}>
+        <Carousels />
+      </Container>
+      <Container className={classes.buttonContainer}>
+          <Button variant="contained" color="primary" disableElevation>
+            Create Project
+          </Button>
+          <Button variant="contained" color="primary" disableElevation>
+            Explore Project
+          </Button>
+        </Container>
+    </>
   );
 };
 
